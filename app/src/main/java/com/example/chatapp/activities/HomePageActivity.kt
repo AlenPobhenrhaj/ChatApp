1e4ae7f3b858bd3c959fa78ee7d88d8c15812f2c
Pobhenrhaj <alenprem4@yahoo.com>
2023-03-29 17:44:45 +0800

    Added HomePage & User List with logo and homepage text(welcome)

--- app/src/main/java/com/example/chatapp/activities/HomePageActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/HomePageActivity.kt
@@ -3,24 +3,51 @@ package com.example.chatapp.activities
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.chatapp.databinding.ActivityHomePageBinding
+import com.example.chatapp.R
+import com.example.chatapp.databinding.ActivityHomePageWithBottomNavBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 
 class HomePageActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityHomePageBinding
+    private lateinit var binding: ActivityHomePageWithBottomNavBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        binding = ActivityHomePageWithBottomNavBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Set up onClick listeners for the buttons
-        binding.buttonChat.setOnClickListener {
-            startActivity(Intent(this, ChatActivity::class.java))
-        }
+        // Set up the bottom navigation view
+        binding.bottomNavigationView.setOnItemSelectedListener(bottomNavListener)
 
-        binding.buttonUserList.setOnClickListener {
-            startActivity(Intent(this, UserListActivity::class.java))
+        auth = FirebaseAuth.getInstance()
+
+        // Set the default screen to ChatActivity
+        /*startActivity(Intent(this, ChatActivity::class.java))*/
+
+        binding.buttonSignOut.setOnClickListener {
+            signOut()
         }
+    }
+
+    private val bottomNavListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.nav_chat -> {
+                startActivity(Intent(this, ChatActivity::class.java))
+                true
+            }
+            R.id.nav_user_list -> {
+                startActivity(Intent(this, UserListActivity::class.java))
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun signOut() {
+        auth.signOut()
+        startActivity(Intent(this, LoginActivity::class.java))
+        finish()
     }
 }
